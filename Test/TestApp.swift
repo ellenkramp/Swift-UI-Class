3b6394c2e92e07e9694645d31e64ba991e215dbd
Ellen <ellen@onuniverse.com>
2021-12-15 06:44:35 -0500

    Initial Commit

---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Ellen Kramp on 12/15/21.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
